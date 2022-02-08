a75e65c833e2d9c972cdc9aa4f3defd85c0edf61
Aleksandr_z <30384395+NickManibus@users.noreply.github.com>
2022-02-08 13:42:54 +0300

    Update hw_03.py

--- Lesson_06/hw_03.py
+++ Lesson_06/hw_03.py
@@ -4,18 +4,3 @@
 # Записать данный словарь на диск в json-файл.
 
 
-import json
-
-b_dict = {
-          '159612': ('Tom', 23),
-          '310640': ('Noah', 32),
-          '044208': ('Jacob', 41),
-          '233648': ('Liam', 21),
-          '053872': ('Michael', 27),
-          '403600': ('Anderson', 54)
-}
-
-with open('name_two.json', 'w') as f:
-    json.dump(b_dict, f)
-with open('name_two.json') as f:
-    print(f.read())
