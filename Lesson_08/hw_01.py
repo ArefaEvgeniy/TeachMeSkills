2f4ec846baaab402067cf50ae4ca56c38c946d14
Avam1n <avamin95@gmail.com>
2022-02-11 20:25:51 +0300

    Added hw_01.py by Nechepa

--- Lesson_08/hw_01.py
+++ Lesson_08/hw_01.py
@@ -4,3 +4,31 @@
 # Методы move и stop выводят сообщение на экран «move» и «stop»,
 # а birthday увеличивает атрибут age на 1.
 # Атрибуты brand, age и mark являются обязательными при объявлении объекта.
+import math
+
+
+class Auto:
+    brand = "Fiat Chrysler Automobiles"
+    age = 2005
+    color = "Black"
+    mark = "Chrysler"
+    weight = 2225
+
+    def move(self):
+        print("MOVE, Man!!!")
+
+    def birthday(self):
+        return self.age + 1
+
+    def stop(self):
+        print("Man, STOP!!!")
+
+
+auto_1 = Auto()
+
+print(f"Brand auto --- {auto_1.brand}", f"Mark --- {auto_1.mark}", sep='\n')
+print()
+auto_1.move()
+auto_1.stop()
+print()
+print(auto_1.birthday())
