a70c7032108b0310789e293088a3abe5757bd441
Avam1n <avamin95@gmail.com>
2022-02-12 18:09:44 +0300

    change HW_01.py

--- Lesson_08/hw_01.py
+++ Lesson_08/hw_01.py
@@ -8,27 +8,33 @@ import math
 
 
 class Auto:
-    brand = "Fiat Chrysler Automobiles"
-    age = 2005
-    color = "Black"
-    mark = "Chrysler"
-    weight = 2225
+    brand = None
+    age = None
+    color = None
+    mark = None
+    weight = None
+
+    def __init__(self, brand, age, mark, weight, color):
+        self.brand = brand
+        self.age = age
+        self.mark = mark
+        self.color = color
+        self.weight = weight
 
     def move(self):
         print("MOVE, Man!!!")
 
     def birthday(self):
-        return self.age + 1
+        self.age += 1
 
     def stop(self):
         print("Man, STOP!!!")
 
 
 if __name__ == '__main__':
-    auto_1 = Auto()
-    print(f"Brand auto --- {auto_1.brand}", f"Mark --- {auto_1.mark}", sep='\n')
-    print()
-    auto_1.move()
-    auto_1.stop()
-    print()
-    print(auto_1.birthday())
+    auto_1 = Auto('Chrysler', 2005, '300C', 2225, 'black')
+    print(auto_1.brand)
+    print(auto_1.age)
+    print(auto_1.mark)
+    print(auto_1.weight)
+    print(auto_1.color)
